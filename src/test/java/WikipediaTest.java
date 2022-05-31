f51689403a9680ff86d229792abf4cdd48bc4644
Weisz Dávid <yaraclen@gmail.com>
2022-05-31 12:10:18 +0200

    wikipedia login test with github

--- src/test/java/WikipediaTest.java
+++ src/test/java/WikipediaTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -6,35 +7,38 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.sql.Driver;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WikipediaTest {
-    private static WebDriver webDriver;
-    ChromeDriver driver = new ChromeDriver();
-    private final String URL = "https://en.wikipedia.org/w/index.php?title=Special:UserLogin";
+    WebDriver driver;
 
-    @BeforeAll
-    static void setDriverProperty() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-    }
-    @BeforeEach
-    void setWebDriver(){
-        webDriver = new ChromeDriver();
-    }
     @Test
     public void loginWikipedia() {
-        webDriver.get(URL);
-        WebDriverWait wait = new WebDriverWait(webDriver, 30);
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--headless");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.navigate().to("https://en.wikipedia.org/w/index.php?title=Special:UserLogin");
         String inputUserName = "Yaraclen";
-        WebElement findUserName = webDriver.findElement(By.id("wpName1"));
+        WebElement findUserName = driver.findElement(By.id("wpName1"));
         findUserName.sendKeys(inputUserName);
         String inputPassword = "TEmp1119";
-        WebElement findPassword = webDriver.findElement(By.id("wpPassword1"));
+        WebElement findPassword = driver.findElement(By.id("wpPassword1"));
         findPassword.sendKeys(inputPassword);
         findPassword.sendKeys(Keys.ENTER);
     }
+}
