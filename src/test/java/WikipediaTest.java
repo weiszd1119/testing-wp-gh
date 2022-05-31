1db12bddb5340f7637578066e0a2c032580d8d8a
Weisz Dávid <yaraclen@gmail.com>
2022-05-31 12:32:58 +0200

    wikipedia all test on github

--- src/test/java/WikipediaTest.java
+++ src/test/java/WikipediaTest.java
@@ -41,4 +41,63 @@ public class WikipediaTest {
         findPassword.sendKeys(inputPassword);
         findPassword.sendKeys(Keys.ENTER);
     }
+    @Test
+    public void searchSeleniumWikipedia() {
+        loginWikipedia();
+        String inputSearch = "Selenium (software)"; //a feladat Selenium-ot ír inputnak, de az csak a lapértelmezőre ugrik
+        WebElement findSearch = driver.findElement(By.id("searchInput"));
+        findSearch.sendKeys(inputSearch);
+        findSearch.sendKeys(Keys.ENTER);
+        assertEquals("https://en.wikipedia.org/wiki/Selenium_(software)",driver.getCurrentUrl());
+        //driver.get(URL);
+    }
+
+    @Test
+    public void searchFrameworkWikipedia() {
+        loginWikipedia();
+        String inputSearch = "Framework";
+        WebElement findSearch = driver.findElement(By.id("searchInput"));
+        findSearch.sendKeys(inputSearch);
+        findSearch.sendKeys(Keys.ENTER);
+        WebElement clickOnLink = driver.findElement(By.id("mw-content-text"));
+        clickOnLink.click();
+        assertEquals("https://en.wikipedia.org/wiki/Framework", driver.getCurrentUrl());
+        //webDriver.get(URL);
+    }
+
+    @Test
+    public void setFontStyle() throws InterruptedException {
+        loginWikipedia();
+        WebElement clickOnLanguageSettings = driver.findElement(By.xpath("//*[@id=\"p-lang\"]/button"));
+        clickOnLanguageSettings.click();
+        Thread.sleep(2000); //nem találja a gombot, mert még nem töltött be
+        WebElement clickOnFonts = driver.findElement(By.cssSelector("#uls-display-settings-fonts-tab"));
+        clickOnFonts.click();
+        Thread.sleep(2000);
+        WebElement clickOnCheckbox = driver.findElement(By.id("webfonts-enable-checkbox"));
+        clickOnCheckbox.click();
+        //Font selection
+        /*
+        Thread.sleep(2000);
+        WebElement clickOnSelectedFont = driver.findElement(By.id("content-font-selector"));
+        Thread.sleep(2000);
+        Selectfont selectfont = new Selectfont(clickOnSelectedFont);
+        selectfont.selectByVisibleText("ComicNeue");
+        Thread.sleep(2000);
+        WebElement clickOnApply = webDriver.findElement(By.xpath("//*[@id=\"language-settings-dialog\"]/div[3]/div/button[2]"));
+        clickOnApply.click();
+        */
+    }
+
+    @Test
+    public void findAParagraph() {
+        loginWikipedia();
+        String inputSearch = "Selenium (software)";
+        WebElement findSearch = driver.findElement(By.id("searchInput"));
+        findSearch.sendKeys(inputSearch);
+        findSearch.sendKeys(Keys.ENTER);
+        WebElement clickOnLinkSeleniumGrid = driver.findElement(By.xpath("//*[@id=\"toc\"]/ul/li[2]/ul/li[5]/a"));
+        clickOnLinkSeleniumGrid.click();
+        assertEquals("https://en.wikipedia.org/wiki/Selenium_(software)#Selenium_Grid", driver.getCurrentUrl());
+    }
 }
